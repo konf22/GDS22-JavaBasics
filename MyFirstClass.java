b8c4b8d10144d1ed5afb4fdfa27c8ac1f144618b
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-09-23 16:47:53 +0200

    My First Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+
+    // wir haben unsere erste Klasse erzeugt
+
+    //psvm
+    public static void main(String[] args) {
+        // erste Ausgabe
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        // args
+        // kürzel sout + tab
+        System.out.println(Arrays.toString(args));
+    }
+}
