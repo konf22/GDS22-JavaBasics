ea32f2c1368c3fc27ea1bd2ffa27877acf0d0f1f
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-09-23 18:11:18 +0200

    Variables and Comments

---
+++ Comments.java
@@ -0,0 +1,24 @@
+public class Comments {
+
+    // /** schreiben und enter
+
+    /**
+     * ich bin ein javadoc Kommentar
+     * @param args Parameter für den Programmaufruf
+     */
+    public static void main(String[] args) {
+
+        // Ich bin ein Zeilenkommentar
+        // ich bin nur für eine Zeile gültig
+
+        String lastName = "Mustermann"; // Nachname deklariert und initialisiert
+
+
+        // Blockkommentar
+
+        /*
+        Ich bin ein Blockkommentar
+        Ich gehe über mehrere Zeilen
+         */
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,37 @@
+public class Variables {
+
+    // Erstellung des Einstiegspunkts
+    public static void main(String[] args) {
+        // Variablen Deklaration
+        String firstName;
+        int x, y, z;
+        System.out.println();
+
+        // initialisieren
+        firstName = "Daniel";
+        x = 1;
+        y = 2;
+        z = 3;
+
+        // Deklaration + Init
+        char a = 'a';
+        int p = 2, q = 3;
+
+        // variablen wert tauschen
+        firstName = "Max";
+
+        // anderer Datentyp => fehler
+        //firstName = 1;
+
+
+        var firstName2 = firstName;
+
+        var byteDeklaration = 1;
+
+        var doubleNr = 4000;
+
+        // addieren
+        int addition = 1 + 3;
+
+    }
+}
