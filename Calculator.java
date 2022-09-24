8eafdeb711c9d9672aa1562cca5ca06640def2b0
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-09-24 14:55:03 +0200

    first if

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        // Variablen deklarieren und initialisieren
+        /*// Variablen deklarieren und initialisieren
         String name = "MyFirstCalculator";
         int p1 = 2;
         int p2 = 3;
@@ -23,6 +23,37 @@ public class Calculator {
         System.out.println("ergebnis2 = " + ergebnis2);
 
         // Berechnung des Ergebnisses bei aufruf von println
-        System.out.println("Ergebnis2: " + (p1 + p2));
+        System.out.println("Ergebnis2: " + (p1 + p2));*/
+
+        int a = 1;
+        int b = 2;
+        int ergebnis = add(a,b);
+        System.out.println("ergebnis = " + ergebnis);
+        System.out.println("subtrahieren: " + sub(2, 0));
+        int result = power(2);
+        System.out.println(div(2,0.0));
+    }
+
+    public static int add(int input1, int input2) {
+        int result = input1 + input2;
+        return result;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static double div(int a, double b) {
+        if (b == 0) {
+            System.out.println("Durch 0 kann nicht dividiert werden");
+            return 0;
+        }
+        double result = a / b;
+        return result;
+    }
+
+    public static int power(int a) {
+        int result = a * a;
+        return result;
     }
 }
