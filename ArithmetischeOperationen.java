c2c5ab84c7bfba00de8dab2143243cd32206fe55
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-09-24 12:03:30 +0200

    Pre- und Post Inkrement

--- ArithmetischeOperationen.java
+++ ArithmetischeOperationen.java
@@ -33,5 +33,16 @@ public class ArithmetischeOperationen {
         // Restwert - Modulo
         result = a % b;
         System.out.println("result modulo = " + result);
+
+        // a++ => bedeutet a = a + 1
+        System.out.println("a++");
+        // ich erwarte mir 3
+        System.out.println(a++);
+        System.out.println("a = " + a); // ich erwarte mir 4
+
+        // ++b
+        System.out.println("++b");
+        System.out.println(++b); // ich erwarte mir 3
+        System.out.println("b = " + b); // ich erwarte mir 3
     }
 }
